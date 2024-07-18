5c2ca57cbcea35e31bd0a676d0a5c6f116d29b61
Phantom804 <asimarain184@gmail.com>
2024-07-18 15:43:39 +0500

    added env sample

---
+++ README.md.txt
@@ -0,0 +1,32 @@
+
+# Digital E-commerce
+
+A digital e-commerce platform for buying courses and website templates.
+
+
+## Demo
+
+https://mern-ecommerce-frontend-black.vercel.app/
+
+
+## Setup .env file
+
+Set up the environment variables given in the .env.sample file.
+
+
+## Run Locally
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+
+Start the app
+
+```bash
+  npm run dev
+```
+
+
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -3,7 +3,7 @@ import { useCart } from "../context/CartContext";
 import axios from "axios";
 import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe('pk_test_51OmFwJBf5bICFbSVEKPXaEFPebMF8405tWdaNTKTzk1zWhE6yU0DwWccXHLh8klYvaMlHZfJ25k6JEWf5XGUnVcD001LhCkw9j');
+const stripePromise = loadStripe(import.meta.env.VITE_REACT_APP_PUBLISHABLE_KEY);
 
 const Cart = () => {
     const { cartItems, decreaseCartItemQuantity, addToCart, clearCart } = useCart();
@@ -15,20 +15,20 @@ const Cart = () => {
 
     const totalPrice = cartItems.reduce((acc, item) => acc + item.priceInCents * item.quantity, 0);
 
-    
 
 
-const handleCheckout = async () => {
-    const stripe = await stripePromise;
 
-    const transformedItems = cartItems.map(item => ({
-        name: item.name,
-        priceInCents: item.priceInCents,
-        quantity: item.quantity,
-        image: item.image
-    }));
+    const handleCheckout = async () => {
+        const stripe = await stripePromise;
 
-    try {
+        const transformedItems = cartItems.map(item => ({
+            name: item.name,
+            priceInCents: item.priceInCents,
+            quantity: item.quantity,
+            image: item.image
+        }));
+
+        try {
             const response = await axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/stripe/create-checkout-session`, {
                 products: transformedItems
             });
@@ -46,34 +46,34 @@ const handleCheckout = async () => {
     };
 
 
-  return (
-    <div className='p-4 mt-16 max-w-[1400px] mx-auto'>
-        <h2 className='text-2xl font-semibold text-center my-6'>Shopping Cart</h2>
+    return (
+        <div className='p-4 mt-16 max-w-[1400px] mx-auto'>
+            <h2 className='text-2xl font-semibold text-center my-6'>Shopping Cart</h2>
 
-        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6'>
-            {cartItems.map((item, index) => (
-                <div key={index} className='bg-base-200 rounded-lg shadow-lg p-4 flex flex-col'>
-                    <img src={item.image} alt={item.name} className='rounded-md mb-4 w-full h-64 object-cover' />
-                    <h2 className='text-lg font-bold mb-2'>{item.name}</h2>
-                    <p className='text-md mb-1'>Price: ${(item.priceInCents / 100).toFixed(2)}</p>
-                    <div className='flex items-center justify-between text-md mb-3'>
-                        <p>Quantity: {item.quantity}</p>
-                        <div className='flex items-center'>
-                            <button onClick={() => decreaseCartItemQuantity(item._id)}
-                            className='  font-bold hover:underline'
-                            >Remove</button>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                {cartItems.map((item, index) => (
+                    <div key={index} className='bg-base-200 rounded-lg shadow-lg p-4 flex flex-col'>
+                        <img src={item.image} alt={item.name} className='rounded-md mb-4 w-full h-64 object-cover' />
+                        <h2 className='text-lg font-bold mb-2'>{item.name}</h2>
+                        <p className='text-md mb-1'>Price: ${(item.priceInCents / 100).toFixed(2)}</p>
+                        <div className='flex items-center justify-between text-md mb-3'>
+                            <p>Quantity: {item.quantity}</p>
+                            <div className='flex items-center'>
+                                <button onClick={() => decreaseCartItemQuantity(item._id)}
+                                    className='  font-bold hover:underline'
+                                >Remove</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
+
+            <div className='text-center mt-8'>
+                <p className='text-2xl font-semibold mb-4'>Total Price: ${(totalPrice / 100).toFixed(2)}</p>
+                <button onClick={handleCheckout} className='btn btn-accent'>Proceed to Checkout</button>
+            </div>
         </div>
-        
-        <div className='text-center mt-8'>
-            <p className='text-2xl font-semibold mb-4'>Total Price: ${(totalPrice / 100).toFixed(2)}</p>
-            <button onClick={handleCheckout} className='btn btn-accent'>Proceed to Checkout</button>
-        </div>  
-    </div>
-  )
+    )
 }
 
 export default Cart
